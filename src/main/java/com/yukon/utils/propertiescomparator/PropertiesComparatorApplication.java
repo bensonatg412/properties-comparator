657dc200162d1a0d601d612c7cb5e5aed2110373
Jenya Vasylynchuk <bensonatg412@gmail.com>
2019-07-08 12:35:44 +0300

    SpringBoot Configured

---
+++ src/main/java/com/yukon/utils/propertiescomparator/PropertiesComparatorApplication.java
@@ -0,0 +1,23 @@
+package com.yukon.utils.propertiescomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.*;
+
+@SpringBootApplication
+public class PropertiesComparatorApplication {
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(PropertiesComparatorApplication.class, args);
+		Map<String, Properties> properties = new HashMap<>();
+		Map<String, Object> keys = new HashMap<>();
+		Map<String, List<String>> forgotKeys = new HashMap<>();
+		PropertiesComparator.loadProperties(keys, properties);
+		PropertiesComparator.checkMissingKeys(keys, properties, forgotKeys);
+		PropertiesComparator.printAllValues(keys, properties);
+		PropertiesComparator.printMissingValues(forgotKeys, properties);
+	}
+
+}
